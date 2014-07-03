8b474a0f1d8e1674f4815f105a10839cf69f645b
Solal Pirelli <solal.pirelli@gmail.com>
2014-07-03 17:44:07 +0200

    Using getTotalLife in getThread
    Avoid going back-and-forth between attacks since getLife changes the threat after an attack.

--- getPreferredTarget.js
+++ getPreferredTarget.js
@@ -1,28 +1,36 @@
-// Computes the threat, from 0 to 1, from an enemy
-function getThreat(enemy) {
-	// * 1.0 to use floats
-	return (getForce(enemy) * 1.0) / (getLife(enemy) * 1.0);
+/// --------------- ///
+/// Enemy targeting ///
+/// --------------- ///
+
+/// Computes the threat, from 0 to 1, from an enemy
+function getThreat( enemy ) {
+  return ( getForce( enemy ) * 1.0 ) / ( getTotalLife( enemy ) * 1.0 );
 }
 
-// Gets the preferred target, given a shield threshold (absolute shield); targets with a higher shield will be ignored
-function getTarget(shieldThreshold) {
-	var target = null;
-	var foundTarget = false;
-	var minShield = 1000000;
-	var maxThreat = -1;
-	for (var enemy in getAliveEnemies()) {
-		var shield = getAbsoluteShield(enemy);
-		var threat = getThreat(enemy);
-		if (shield <= shieldThreshold) {
-			foundTarget = true;
-			if (maxThreat < threat) {
-				target = enemy;
-				maxThreat = threat;
-			}
-		} else if (!foundTarget && shield < minShield) {
-			target = enemy;
-		}
-		minShield = min(minShield, shield);
+/// Gets the most threatening target that has a shield lower than the specified threshold,
+/// or the target with the smallest shield if no targets have such a shield
+function getTarget( shieldThreshold ) {
+  if( getEnemiesCount() === 1 ) {
+    return getEnemies()[0];
+  }
+
+  var target = null;
+  var foundTarget = false;
+  var minShield = 1000000;
+  var maxThreat = -1;
+  for( var enemy in getAliveEnemies() ) {
+    var shield = getAbsoluteShield( enemy );
+	var threat = getThreat( enemy );
+    if( shield <= shieldThreshold ) {
+	  foundTarget = true;
+	  if( maxThreat < threat ) {    
+	    target = enemy;
+		maxThreat = threat;
+	  }
+	} else if( !foundTarget && shield < minShield ) {
+	  target = enemy;
 	}
-	return target;
+	minShield = min( minShield, shield );
+  }
+  return target;
 }

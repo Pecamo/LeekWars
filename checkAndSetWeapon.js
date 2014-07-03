5c53bb894f0304e39edd7253ca0e7454b7352b33
Solal Pirelli <solal.pirelli@gmail.com>
2014-07-03 17:42:48 +0200

    Added a simpler alternative to checkAndSetWeapon

--- checkAndSetWeapon.js
+++ checkAndSetWeapon.js
@@ -1,6 +1,9 @@
-//Checks if the weapon is already equipped and equips it if not
-function checkAndSetWeapon(weapon) {
-	if (getWeapon() != weapon) {
-		setWeapon(weapon);
-	}
+// Simple version if you only use one weapon:
+global ___ = setWeapon( THE_WEAPON_YOU_WANT );
+
+/// Equips the specified weapon if it isn't already.
+function checkAndSetWeapon( weapon ) {
+  if( getWeapon() != weapon ) {
+    setWeapon( weapon );
+  }
 }
